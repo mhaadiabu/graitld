fbb4ded9985a4b61f2d3cbfd8641fa0b74a09350
mhaadiabu <mhabubakar999@outlook.com>
2026-02-08 14:04:39 +0000

    feat: add sidebar

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,68 @@
+import Link from "next/link";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "./ui/sidebar";
+
+const sidebarItems = [
+  {
+    title: 'Overview',
+    href: '/',
+  },
+  {
+    title: 'Influencers',
+    href: '/influencers',
+  },
+  {
+    title: 'Analytics',
+    href: '/analytics',
+  },
+  {
+    title: 'Reports',
+    href: '/reports',
+  },
+  {
+    title: 'Channel Lookup',
+    href: '/channel-lookup',
+  },
+  {
+    title: 'Settings',
+    href: '/settings',
+  },
+]
+
+export const AppSidebar = () => {
+  return (
+    <Sidebar>
+      {/*<SidebarHeader>
+
+      </SidebarHeader>*/}
+
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            {sidebarItems.map(({href, title}) => (
+              <SidebarMenuItem key={title}>
+                <SidebarMenuButton>
+                  <Link href={href}>{title}</Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>)}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton>Sign Out</SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+};
